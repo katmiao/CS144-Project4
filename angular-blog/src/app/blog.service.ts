fa784c9122d126dfc51fca2e3e4990169a10595a
Katherine Miao <katmiao99@gmail.com>
2020-05-18 01:46:50 -0700

    starting blog.service.ts

--- angular-blog/src/app/blog.service.ts
+++ angular-blog/src/app/blog.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
-export class BlogService {
 
-  constructor() { }
+export class BlogService {
+	draft: Post;
+
+	constructor() { 
+		this.draft = null;
+	}
+
+	setCurrentDraft(post: Post): void {
+		this.draft = post;
+	}
+
+	getCurrentDraft(): Post {
+		return this.draft;
+	}
+}
+
+export class Post {
+	postid: number;
+	created: Date;
+	modified: Date;
+	title: string;
+	body: string;
 }
